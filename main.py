e925317ad73f076ba534a02d5ac245395a90ea8f
katushenca <shutenko.katya@bk.ru>
2024-11-14 18:37:29 +0500

    add output parser and triangle class
    output_parser:
     - get_triangle_pattern
     - fill_triangle
    Triangle:
     - get_free_positions

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import input_parser
+from input_parser import InputParser
+import Triangle
+from output_parser import OutputParser
 
-while True:
-    try:
-        parser = input_parser.InputParser.get_input_data()
-    except Exception as e:
-        print(f"Некорректные входные данные: {e.args[0]}")
-        continue
-    break
+def get_input_data() -> InputParser:
+    while True:
+        try:
+            parser = InputParser.get_input_data()
+            return parser
+        except Exception as e:
+            print(f"Некорректные входные данные: {e.args[0]}")
+            continue
+
+def get_filled_triangle():
+    parser = get_input_data()
+    triangle = Triangle.Triangle(parser.triangle_len, parser.nodes_count)
+    o_parser = OutputParser()
+    triangle.ascii_triangle = o_parser.fill_triangle(triangle)
+    return triangle
+
+triangle = get_filled_triangle()
+print(*triangle.ascii_triangle)
---
+++ output_parser.py
@@ -0,0 +1,22 @@
+import Triangle
+import random
+
+class OutputParser:
+    @staticmethod
+    def get_triangle_pattern(triangle_len : int) -> list[str]:
+        with open(f'triangle_patterns/{triangle_len}.txt') as file:
+            triangle_str = file.readlines()
+        return triangle_str
+
+    def fill_triangle(self, triangle : Triangle) -> list[str]:
+        free_spaces : list[tuple] = triangle.get_free_positions()
+        triangle_ascii = triangle.ascii_triangle
+        for i in range(triangle.nodes_count):
+            random_place = random.choice(free_spaces)
+            triangle_ascii[random_place[0]] = triangle_ascii[random_place[0]][:random_place[1]] + str(i) + triangle_ascii[random_place[0]][1 + random_place[1]: ]
+            free_spaces.remove(random_place)
+            random_place = random.choice(free_spaces)
+            triangle_ascii[random_place[0]] = triangle_ascii[random_place[0]][:random_place[1]] + str(i) + triangle_ascii[random_place[0]][1 + random_place[1]: ]
+            free_spaces.remove(random_place)
+        return triangle_ascii
+
